de56848fa94e94051a4a8d10b24078d83254785e
Ivan Sopov <isopov@reveredata.com>
2013-03-10 18:59:56 +0200

    using normal laytout for the login page

--- src/main/java/com/sopovs/moradanen/fan/controller/LoginController.java
+++ src/main/java/com/sopovs/moradanen/fan/controller/LoginController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class LoginController {
+public class LoginController extends AbstractController {
 
-	@RequestMapping("login")
-	public String loginForm() {
-		return "loginForm";
-	}
+    @RequestMapping("login")
+    public String loginForm() {
+        return "loginForm";
+    }
 
-	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
-	public String loginerror(ModelMap model) {
-		model.addAttribute("error", "true");
-		return "loginForm";
-	}
+    @RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
+    public String loginerror(ModelMap model) {
+        model.addAttribute("error", "true");
+        return "loginForm";
+    }
 
 }
