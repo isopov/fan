5b4084bae6ba0633dc5ff5d74febef7868a391f6
Ivan Sopov <sopov.ivan@gmail.com>
2013-12-23 22:22:06 +0200

    fix for using undertow instead of tomcat

--- fan-web/src/main/java/com/sopovs/moradanen/fan/FanApplication.java
+++ fan-web/src/main/java/com/sopovs/moradanen/fan/FanApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.embedded.ErrorPage;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 
 import com.google.common.collect.ImmutableSet;
+import com.sopovs.moradanen.spring.boot.autoconfigure.web.UndertowEmbeddedServletContainerFactory;
 
 @Configuration
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
@@ -18,19 +18,22 @@ import com.google.common.collect.ImmutableSet;
 public class FanApplication {
 
     @Bean
-    public TomcatEmbeddedServletContainerFactory tomcatFactory() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+    public UndertowEmbeddedServletContainerFactory undertowFactory() {
+
+        UndertowEmbeddedServletContainerFactory factory = new
+                UndertowEmbeddedServletContainerFactory();
         ErrorPage notFoundPage = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
         ErrorPage forbiddenPage = new ErrorPage(HttpStatus.FORBIDDEN, "/403");
-        factory.setErrorPages(ImmutableSet.of(notFoundPage, forbiddenPage));
+        ErrorPage badRequest = new ErrorPage(HttpStatus.BAD_REQUEST, "/error");
+        factory.setErrorPages(ImmutableSet.of(notFoundPage, forbiddenPage, badRequest));
         return factory;
     }
 
     // This does not work
-    @Bean
-    public ErrorPage error404() {
-        return new ErrorPage(HttpStatus.NOT_FOUND, "/404");
-    }
+    // @Bean
+    // public ErrorPage error404() {
+    // return new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+    // }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(FanApplication.class, args);
